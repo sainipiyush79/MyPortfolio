ba13110c64803f7ca72e6c703347daf3647dff54
Piyush Saini <piyushsaini@dhcp-10-4-7-2.wireless.rochester.edu>
2021-01-10 17:03:23 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+
+import MyNavbar from './componenets/my-navbar/mynavbar.component'
+
+const App = () => {
+  return (
+    <div>
+      <MyNavbar/>
+      My Portfolio</div>
+  );
+}
+
+export default App;
