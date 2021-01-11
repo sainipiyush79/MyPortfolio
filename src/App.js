ec2902d6e3aa7423b7c0305ecdd62c12c66572d6
Piyush Saini <piyushsaini@dhcp-10-4-7-2.wireless.rochester.edu>
2021-01-11 00:52:57 -0500

    added about and skill page

--- src/App.js
+++ src/App.js
@@ -3,12 +3,48 @@ import './App.css';
 
 
 import MyNavbar from './componenets/my-navbar/mynavbar.component'
+import MyCarousal from './componenets/my-carousal/my-carousal.component'
+import MyTitleMessage from "./componenets/title-message/title-message.component";
+import About from "./pages/about/about.component";
+import Container from "react-bootstrap/Container";
+import { Parallax } from "react-parallax";
+import Fade from 'react-reveal/Fade';
+import Skills from "./pages/skills/skills.component";
+import Slide from "react-reveal/Slide";
+
 
 const App = () => {
   return (
     <div>
       <MyNavbar/>
-      My Portfolio</div>
+      <MyCarousal/>
+      <MyTitleMessage /> 
+
+
+      <div>
+        <Parallax
+          blur={{ min: -30, max: 30 }}
+          bgImage={require("./assets/img/parallex/background.webp")}
+          bgImageAlt=""
+          strength={-200}
+        >
+          <div>
+            <Container className="container-box rounded">
+              <Fade duration={500}>
+                <About />
+              </Fade>
+            </Container>
+          </div>
+        </Parallax>
+      </div>
+      {/* adding skill set */}
+      <Container className="container-box rounded">
+        <Slide bottom duration={500}>
+          <hr />
+          <Skills />
+        </Slide>
+      </Container>
+     </div>
   );
 }
 
